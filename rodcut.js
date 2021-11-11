40a988615f0363830eff1b0b5cb61aca34005131
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-10 20:44:17 -0800

    not broken for now

--- rodcut.js
+++ rodcut.js
@@ -3,38 +3,45 @@
 //long into integer-length pieces if the sale price of a piece i units long is 
 //p of i for i = 1 ,2, ....n. 
 //What are the time and spece efficiencies of your algo?
-//what is p
 
 //not sure this is memoized
 function btmUprodCut(p, n)
 {
     let r = Array(n);
-    for(let i = 0; i < n; i++)
+    for(let i = 0; i <= n; i++) //is this the right size
     {
-        r[i] = -null; //js might have a preferred null
+        r[i] = null; //js might have a preferred null
     }
     r[0] = 0;
     
-    for(let j = 0; j < n; j++)
+    for(let j = 0; j <= n; j++)
     {
-        q = -null;
+        q = null;
         for(let i = 0; i < j; i++)
         {
             q = Math.max(q, p[i] + r[j - i]);
         }
         r[j] = q
     }
+
+    console.log("pringint our array");
+    let i = 0;
+    while(i <= n) //n val is undefined
+    {
+        console.log(r[i], " ");
+        i += 1;
+    }
+
     return r[n]
 }
 
 //optimal rodcut
-
 function printVals(p, n)
 {
     //produce arrays r and s, which hold the values
 }
 
-let p = [1, 2, 3, 4, 5, 2, 1];
+let p = [1, 2, 3, 4, 5];
 let n = 4;
 
 let answer = btmUprodCut(p, n);
