4c03352bfd82e8cbf27a388311480bee8931a540
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-12 20:58:15 -0800

    done

--- rodcut.js
+++ rodcut.js
@@ -4,7 +4,6 @@
 //p of i for i = 1 ,2, ....n. 
 //What are the time and spece efficiencies of your algo?
 //import performance
-
 //memoized
 function btmUprodCut(p, n)
 {
@@ -23,31 +22,31 @@ function btmUprodCut(p, n)
         }
         r[j] = q
     }
-    console.log("pringint our array :");
-    let i = 0;
-    while(i <= n) //n val is undefined
-    {
-        console.log(r[i], " ");
-        i += 1;
-    }
-    console.log("done printing array");
     return r[n]
 }
-
 function test()
 {
-    let n = 10;
-    let p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
+    let n = 45; //we are testing 5, 20, 45
+    //let p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
+    let p = [1, 5, 6, 7 , 8, 9, 7 , 8, 9, 10, 17, 10, 17, 17, 20, 23, 24, 25, 26, 27, 28, 29,7 , 8, 9, 10, 17, 30, 32, 34, 35, 36, 38, 40, 42, 44 ,46, 50, 52, 56];
+    //let p = [1, 5, 8, 9];
+    //[0, 1, 5, 8, 9 10]
+    //[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
     //for(let i = 0; i < p.length; i++)
     //{
     //    console.log(i, "th price is ", p[i]);
     //}
-    let startTime = performance.now();
-    let btmupanswer = btmUprodCut(p, n);
-    let endTime = performance.now();
-    let delta =  endTime - startTime;
-    console.log("n is ", n, "answer is ", btmupanswer, "elapsed time is ", delta, " millisecconds");
-    let answer = (p, n);
+    //for(let i = 0; i < 10; i++)
+    //{
+      let startTime = performance.now();
+      let btmupanswer = btmUprodCut(p, n);
+      let endTime = performance.now();
+      let delta =  endTime - startTime;
+      console.log("n is ", n, "answer is ", btmupanswer, "elapsed time is ", delta, " millisecconds");
+      //let answer = (p, n);
+      //n += 1;
+    //}
     //for(let i = 0; i < 10; i++)
     //{
     //    let answer = btmUprodCut(p, n);
@@ -55,4 +54,5 @@ function test()
     //    n += 1;
     //}
 }
+
 test();
